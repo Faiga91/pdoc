4f6cb84604f1799c498e36181fe3620f120ce61a
Maximilian Hils <git@maximilianhils.com>
2022-12-27 17:00:00 +0100

    update test env dependencies

--- test/freeze-requirements.sh
+++ test/freeze-requirements.sh
@@ -4,7 +4,7 @@ cd -- "$(dirname -- "$0")"
 
 rm -rf freeze-venv
 python3.7 -m venv freeze-venv
-freeze-venv/bin/python -m pip install -U pip
+freeze-venv/bin/python -m pip install -U pip setuptools
 freeze-venv/bin/pip install -e ..[dev]
 freeze-venv/bin/pip freeze --all --exclude-editable > ../requirements-dev.txt
 rm -rf freeze-venv
